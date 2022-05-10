e186fa0327e60cc1403d839ca7d12bd054409a73
boat-naruebes@outlook.com <69248790+boat-naruebes@users.noreply.github.com>
2022-05-10 16:36:57 +0700

    change all javascript(v2)

--- javascript/wk2/script.js
+++ javascript/wk2/script.js
@@ -1,29 +1,9 @@
-var white = document.getElementById('white');
-var red = document.getElementById('red');
-var violet = document.getElementById('violet');
-var lightyellow = document.getElementById('lightyellow');
-var yellow = document.getElementById('yellow');
-var green = document.getElementById('green');
-
-
-white.addEventListener('click', function(){
-    white.style.backgroundColor = 'white';
-    console.log(white);
-});
-red.addEventListener('click', function(){
-    white.style.backgroundColor = 'red';
-    console.log(white);
-});
-violet.addEventListener('click', function(){
-    white.style.backgroundColor = 'violet';
-    console.log(violet);
-});
-lightyellow.addEventListener('click', function(){
-    white.style.backgroundColor = '#F5DEB3';
-});
-yellow.addEventListener('click', function(){
-    white.style.backgroundColor = 'yellow';
-});
-green.addEventListener('click', function(){
-    white.style.backgroundColor = 'green';
-});
+var color = document.querySelectorAll('.click')
+for(var i = 0; i < color.length; i++){
+    color[i].classList.remove('click'); // remove class click
+    color[i].addEventListener('click', function(){
+        const txt = getComputedStyle(this);
+        console.log(txt.backgroundColor) // get all style
+        document.body.style.backgroundColor = txt.backgroundColor;
+    });
+}
